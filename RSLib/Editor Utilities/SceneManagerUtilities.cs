65bf191d050e9ec83c0da462c1a1c5c2e2ab84f9
Six Robin <six.robin@hotmail.fr>
2021-04-10 15:50:38 +0200

    Mod method, scene dirty marker.

---
+++ RSLib/Editor Utilities/SceneManagerUtilities.cs
@@ -0,0 +1,12 @@
+﻿#if UNITY_EDITOR
+namespace RSLib.EditorUtilities
+{
+    public static class SceneManagerUtilities
+    {
+        public static void SetCurrentSceneDirty()
+        {
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
+        }
+    }
+}
+#endif
--- RSLib/Scripts/Helpers.cs
+++ RSLib/Scripts/Helpers.cs
@@ -92,5 +92,18 @@
         }
 
         #endregion MISC
+
+        #region MODULO
+
+        /// <summary>Custom modulo operating method to handle negative values.</summary>
+        /// <param name="a">First operand.</param>
+        /// <param name="n">Second operand.</param>
+        /// <returns>Modulo result.</returns>
+        public static int Mod(int a, int n)
+        {
+            return (a % n + n) % n;
+        }
+
+        #endregion
     }
 }
