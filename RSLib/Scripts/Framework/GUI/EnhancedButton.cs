e7685bdb40caa7a706ac88cb13fc965e44a4c9c9
Six Robin <six.robin@hotmail.fr>
2021-06-25 13:34:00 +0200

    EnhancedButton now handles selected events.

--- RSLib/Scripts/Framework/GUI/EnhancedButton.cs
+++ RSLib/Scripts/Framework/GUI/EnhancedButton.cs
@@ -50,6 +50,22 @@
             }
         }
 
+        /// <summary>Mimics the behaviour of OnPointerEnter event specified in the Button component when Selectable is selected by joystick navigation.</summary>
+        /// <param name="eventData">Navigation event data.</param>
+        public override void OnSelect(UnityEngine.EventSystems.BaseEventData eventData)
+        {
+            base.OnSelect(eventData);
+            OnPointerEnter(null);
+        }
+
+        /// <summary>Mimics the behaviour of OnPointerExit event specified in the Button component when Selectable is selected by joystick navigation.</summary>
+        /// <param name="eventData">Navigation event data.</param>
+        public override void OnDeselect(UnityEngine.EventSystems.BaseEventData eventData)
+        {
+            base.OnDeselect(eventData);
+            OnPointerExit(null);
+        }
+
         public override void OnPointerEnter(UnityEngine.EventSystems.PointerEventData eventData)
         {
             base.OnPointerEnter(eventData);
