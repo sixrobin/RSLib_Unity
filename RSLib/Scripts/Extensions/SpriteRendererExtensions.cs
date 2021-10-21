9cfe4e717c4adc21376c60429a8a2b8c033e0b01
Robin Six <six.robin@hotmail.fr>
2021-10-21 22:59:04 +0200

    Sprite Renderer extensions file.

---
+++ RSLib/Scripts/Extensions/SpriteRendererExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace RSLib.Extensions
+{
+	using UnityEngine;
+
+	public static class SpriteRendererExtensions
+	{
+		#region FLIP
+
+		/// <summary>
+		/// Flips the SpriteRenderer on the X axis.
+		/// </summary>
+		public static void FlipX(this SpriteRenderer spriteRenderer)
+		{
+			spriteRenderer.flipX = !spriteRenderer.flipX;
+		}
+
+		/// <summary>
+		/// Flips the SpriteRenderer on the Y axis.
+		/// </summary>
+		public static void FlipY(this SpriteRenderer spriteRenderer)
+		{
+			spriteRenderer.flipY = !spriteRenderer.flipY;
+		}
+
+		#endregion FLIP
+	}
+}
