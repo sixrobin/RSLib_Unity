45b7041895bb18f76a12b78e2e923d96fe63db0f
Robin Six <six.robin@hotmail.fr>
2021-10-22 20:12:32 +0200

    Texture2D FlipY and FlipXY methods.

--- RSLib/Scripts/Extensions/Texture2DExtensions.cs
+++ RSLib/Scripts/Extensions/Texture2DExtensions.cs
@@ -4,15 +4,19 @@
 
     public static class Texture2DExtensions
     {
-        #region GENERAL
+        #region FLIP
 
         /// <summary>
-        /// Creates a new Texture2D that is a x-flipped version of the original.
+        /// Creates a new Texture2D that is a X flipped version of the original.
         /// Read/Write must be enabled on the reference texture asset.
         /// </summary>
-        /// <returns>Flipped texture.</returns>
+        /// <returns>Flipped texture in the X axis.</returns>
         public static Texture2D FlipX(this Texture2D original)
         {
+            UnityEngine.Assertions.Assert.IsTrue(
+                original.isReadable,
+                $"Cannot flip Texture2D {original.name} on X since Read/Write has not been checked.");
+
             int w = original.width;
             int h = original.height;
 
@@ -29,6 +33,43 @@
             return flipped;
         }
 
-        #endregion
+        /// <summary>
+        /// Creates a new Texture2D that is a Y flipped version of the original.
+        /// Read/Write must be enabled on the reference texture asset.
+        /// </summary>
+        /// <returns>Flipped texture in the Y axis.</returns>
+        public static Texture2D FlipY(this Texture2D original)
+        {
+            UnityEngine.Assertions.Assert.IsTrue(
+                original.isReadable,
+                $"Cannot flip Texture2D {original.name} on Y since Read/Write has not been checked.");
+
+            int w = original.width;
+            int h = original.height;
+
+            Texture2D flipped = new Texture2D(w, h)
+            {
+                wrapModeU = TextureWrapMode.Clamp
+            };
+
+            for (int x = 0; x < w; ++x)
+                for (int y = 0; y < h; ++y)
+                    flipped.SetPixel(x, h - y - 1, original.GetPixel(x, y));
+
+            flipped.Apply();
+            return flipped;
+        }
+
+        /// <summary>
+        /// Creates a new Texture2D that is a XY flipped version of the original.
+        /// Read/Write must be enabled on the reference texture asset.
+        /// </summary>
+        /// <returns>Flipped texture in the X and Y axis.</returns>
+        public static Texture2D FlipXY(this Texture2D original)
+        {
+            return original.FlipX().FlipY();
+        }
+
+        #endregion FLIP
     }
 }
