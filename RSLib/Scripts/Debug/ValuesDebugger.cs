ba610d63799c987a151987802568ae07849ef6f9
Robin Six <six.robin@hotmail.fr>
2022-01-16 23:10:17 +0100

    Values debugger WIP

---
+++ RSLib/Scripts/Debug/ValuesDebugger.cs
@@ -0,0 +1,137 @@
+﻿namespace RSLib.Debug
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public static class AnchorExtensions
+    {
+        public static bool IsRight(this ValuesDebugger.Anchor anchor)
+        {
+            return anchor == ValuesDebugger.Anchor.LowerRight || anchor == ValuesDebugger.Anchor.UpperRight;
+        }
+
+        public static bool IsUp(this ValuesDebugger.Anchor anchor)
+        {
+            return anchor == ValuesDebugger.Anchor.UpperLeft || anchor == ValuesDebugger.Anchor.UpperRight;
+        }
+    }
+
+    [DisallowMultipleComponent]
+    public sealed class ValuesDebugger : Framework.Singleton<ValuesDebugger>
+    {
+        private const float LINE_HEIGHT = 20f;
+        private const string VALUE_DEBUG_FORMAT = "{0}:{1}";
+
+        public struct ValueEntry
+        {
+            public Dictionary<string, object> Values;
+        }
+
+        [SerializeField, Min(0f)] private float _margin = 0f;
+        [SerializeField, Min(0f)] private float _linesHeight = 15f;
+        [SerializeField] private Color _textsColor = Color.yellow;
+        [SerializeField] private bool _boldFont = true;
+
+        private Dictionary<Anchor, Dictionary<string, object>> _values = new Dictionary<Anchor, Dictionary<string, object>>();
+        private Dictionary<Anchor, GUIStyle> _styles = new Dictionary<Anchor, GUIStyle>();
+
+        public enum Anchor
+        {
+            UpperRight,
+            UpperLeft,
+            LowerRight,
+            LowerLeft
+        }
+
+        public static void DebugValue(string key, object value, Anchor anchor = Anchor.UpperRight)
+        {
+            if (!Instance._values.ContainsKey(anchor))
+                Instance._values.Add(anchor, new Dictionary<string, object>());
+
+            if (Instance._values[anchor].ContainsKey(key))
+                Instance._values[anchor][key] = value;
+            else
+                Instance._values[anchor].Add(key, value);
+        }
+
+        private void InitGUIStyles()
+        {
+            GUIStyle upperLeftStyle = new GUIStyle()
+            {
+                fontStyle = _boldFont ? FontStyle.Bold : FontStyle.Normal,
+                alignment = TextAnchor.UpperLeft
+            };
+
+            GUIStyle upperRightStyle = new GUIStyle()
+            {
+                fontStyle = _boldFont ? FontStyle.Bold : FontStyle.Normal,
+                alignment = TextAnchor.UpperRight
+            };
+
+            GUIStyle lowerLeftStyle = new GUIStyle()
+            {
+                fontStyle = _boldFont ? FontStyle.Bold : FontStyle.Normal,
+                alignment = TextAnchor.LowerLeft
+            };
+
+            GUIStyle lowerRightStyle = new GUIStyle()
+            {
+                fontStyle = _boldFont ? FontStyle.Bold : FontStyle.Normal,
+                alignment = TextAnchor.LowerRight
+            };
+
+            upperLeftStyle.normal.textColor = _textsColor;
+            upperRightStyle.normal.textColor = _textsColor;
+            lowerLeftStyle.normal.textColor = _textsColor;
+            lowerRightStyle.normal.textColor = _textsColor;
+
+            _styles = new Dictionary<Anchor, GUIStyle>()
+            {
+                { Anchor.UpperLeft, upperLeftStyle },
+                { Anchor.UpperRight, upperRightStyle },
+                { Anchor.LowerLeft, lowerLeftStyle },
+                { Anchor.LowerRight, lowerRightStyle }
+            };
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            InitGUIStyles();
+        }
+
+        private void OnGUI()
+        {
+            foreach (KeyValuePair<Anchor, Dictionary<string, object>> values in _values)
+            {
+                Anchor anchor = values.Key;
+                int i = 0;
+                
+                foreach (KeyValuePair<string, object> entry in values.Value)
+                {
+                    Vector2 rectPos = new Vector2()
+                    {
+                        x = _margin,
+                        y = anchor.IsUp() ? (_linesHeight * i + _margin) : (Screen.height - _linesHeight * i - _margin - LINE_HEIGHT)
+                    };
+
+                    Vector2 rectSize = new Vector2()
+                    {
+                        x = Screen.width - (_margin * 2),
+                        y = LINE_HEIGHT
+                    };
+
+                    Rect rect = new Rect(rectPos, rectSize);
+                    GUI.TextField(rect, string.Format(VALUE_DEBUG_FORMAT, entry.Key, entry.Value.ToString()), _styles[values.Key]);
+
+                    i++;
+                }
+            }
+        }
+
+        private void OnValidate()
+        {
+            InitGUIStyles();
+        }
+    }
+}
