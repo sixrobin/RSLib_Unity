ae2190d675b10e8db8d2fa61e63ff877d8f1ede0
Robin Six <six.robin@hotmail.fr>
2022-01-16 23:25:16 +0100

    Values Debugger Clear method

--- RSLib/Scripts/Debug/ValuesDebugger.cs
+++ RSLib/Scripts/Debug/ValuesDebugger.cs
@@ -1,6 +1,7 @@
 ﻿namespace RSLib.Debug
 {
     using System.Collections.Generic;
+    using System.Linq;
     using UnityEngine;
 
     public static class AnchorExtensions
@@ -22,18 +23,15 @@
         private const float LINE_HEIGHT = 20f;
         private const string VALUE_DEBUG_FORMAT = "{0}:{1}";
 
-        public struct ValueEntry
-        {
-            public Dictionary<string, object> Values;
-        }
-
         [SerializeField, Min(0f)] private float _margin = 0f;
         [SerializeField, Min(0f)] private float _linesHeight = 15f;
         [SerializeField] private Color _textsColor = Color.yellow;
         [SerializeField] private bool _boldFont = true;
 
-        private Dictionary<Anchor, Dictionary<string, object>> _values = new Dictionary<Anchor, Dictionary<string, object>>();
+        private Dictionary<Anchor, Dictionary<string, ValueGetter>> _values = new Dictionary<Anchor, Dictionary<string, ValueGetter>>();
         private Dictionary<Anchor, GUIStyle> _styles = new Dictionary<Anchor, GUIStyle>();
+
+        public delegate object ValueGetter();
 
         public enum Anchor
         {
@@ -43,15 +41,29 @@
             LowerLeft
         }
 
-        public static void DebugValue(string key, object value, Anchor anchor = Anchor.UpperRight)
+        public static void DebugValue(string key, ValueGetter valueGetter, Anchor anchor = Anchor.UpperRight)
         {
             if (!Instance._values.ContainsKey(anchor))
-                Instance._values.Add(anchor, new Dictionary<string, object>());
+                Instance._values.Add(anchor, new Dictionary<string, ValueGetter>());
 
             if (Instance._values[anchor].ContainsKey(key))
-                Instance._values[anchor][key] = value;
+                Instance._values[anchor][key] = valueGetter;
             else
-                Instance._values[anchor].Add(key, value);
+                Instance._values[anchor].Add(key, valueGetter);
+        }
+
+        private static void ClearValues()
+        {
+            foreach (KeyValuePair<Anchor, Dictionary<string, ValueGetter>> values in Instance._values)
+            {
+                string[] keys = values.Value.Keys.ToArray();
+                for (int i = keys.Length - 1; i >= 0; --i)
+                {
+                    object target = values.Value[keys[i]].Target;
+                    if (target == null || (target is Object && target.Equals(null)))
+                        values.Value.Remove(keys[i]);
+                }
+            }
         }
 
         private void InitGUIStyles()
@@ -102,12 +114,14 @@
 
         private void OnGUI()
         {
-            foreach (KeyValuePair<Anchor, Dictionary<string, object>> values in _values)
+            ClearValues();
+
+            foreach (KeyValuePair<Anchor, Dictionary<string, ValueGetter>> values in _values)
             {
                 Anchor anchor = values.Key;
                 int i = 0;
                 
-                foreach (KeyValuePair<string, object> entry in values.Value)
+                foreach (KeyValuePair<string, ValueGetter> entry in values.Value)
                 {
                     Vector2 rectPos = new Vector2()
                     {
@@ -122,7 +136,7 @@
                     };
 
                     Rect rect = new Rect(rectPos, rectSize);
-                    GUI.TextField(rect, string.Format(VALUE_DEBUG_FORMAT, entry.Key, entry.Value.ToString()), _styles[values.Key]);
+                    GUI.TextField(rect, string.Format(VALUE_DEBUG_FORMAT, entry.Key, entry.Value().ToString()), _styles[values.Key]);
 
                     i++;
                 }
