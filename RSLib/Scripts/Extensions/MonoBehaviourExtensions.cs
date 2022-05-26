9ac848594b3c5c397ce9431ee555d2787c402cce
Robin Six <55784799+sixrobin@users.noreply.github.com>
2022-05-26 18:13:25 +0200

    Custom coroutine class.

--- RSLib/Scripts/Extensions/MonoBehaviourExtensions.cs
+++ RSLib/Scripts/Extensions/MonoBehaviourExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace RSLib
+﻿namespace RSLib.Extensions
 {
     using UnityEngine;
 
@@ -9,15 +9,15 @@
         /// <summary>
         /// Invokes a callback method after a delay.
         /// </summary>
-        /// <param name="coroutineRunner">MonoBehaviour that will run the delay coroutine.</param>
+        /// <param name="runner">MonoBehaviour that will run the delay coroutine.</param>
         /// <param name="delay">Seconds to wait before callback.</param>
         /// <param name="callback">Callback behaviour.</param>
-        public static void DoAfter(this MonoBehaviour coroutineRunner, float delay, System.Action callback)
+        public static void DoAfter(this MonoBehaviour runner, float delay, System.Action callback)
         {
             if (callback == null)
                 throw new System.Exception($"Coroutine callback cannot be null.");
 
-            coroutineRunner.StartCoroutine(DoAfterCoroutine(delay, callback));
+            runner.StartCoroutine(DoAfterCoroutine(delay, callback));
         }
 
         private static System.Collections.IEnumerator DoAfterCoroutine(float delay, System.Action callback)
