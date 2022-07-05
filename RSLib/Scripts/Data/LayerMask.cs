934a373d114c676f39f2d556b751a0efc47782a6
Robin Six <55784799+sixrobin@users.noreply.github.com>
2022-07-05 23:12:25 +0200

    + DataLayerMask.

---
+++ RSLib/Scripts/Data/LayerMask.cs
@@ -0,0 +1,26 @@
+﻿namespace RSLib.Data
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "New Data LayerMask", menuName = "RSLib/Data/LayerMask")]
+    public class LayerMask : ScriptableObject
+    {
+        [SerializeField] private UnityEngine.LayerMask _mask = 0;
+
+        public UnityEngine.LayerMask Mask => _mask;
+        
+#region CONVERSION OPERATORS
+        
+        public static implicit operator UnityEngine.LayerMask(LayerMask dataLayerMask)
+        {
+            return dataLayerMask.Mask;
+        }
+
+        public static implicit operator int(LayerMask dataLayerMask)
+        {
+            return (int)dataLayerMask.Mask;
+        }
+        
+#endregion // CONVERSION OPERATORS
+    }
+}
