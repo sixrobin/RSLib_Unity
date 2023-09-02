50051ba0811b8b64084fc9b3c7c8b1dcd03473b2
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-09-02 14:42:59 +0200

    Added AnimationEventCallback class.

---
+++ RSLib/Scripts/AnimationEventCallback.cs
@@ -0,0 +1,26 @@
+namespace RSLib
+{
+    #if ODIN_INSPECTOR
+    using Sirenix.OdinInspector;
+    #endif
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public sealed class AnimationEventCallback : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEvent _callback;
+
+        /// <summary>
+        /// Method calling a simple UnityEvent.
+        /// Designed to be called from an animation clip event.
+        /// </summary>
+        #if ODIN_INSPECTOR
+        [Button]
+        #endif
+        public void InvokeCallback()
+        {
+            this._callback?.Invoke();
+        }
+    }
+}
