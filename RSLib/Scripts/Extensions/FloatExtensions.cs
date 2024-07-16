787548e44a490510dcc8e26d6168fb4f3f79e6d2
Robin Six <six.robin@hotmail.fr>
2024-07-16 16:29:06 +0200

    Added Frac to FloatExtensions

--- RSLib/Scripts/Extensions/FloatExtensions.cs
+++ RSLib/Scripts/Extensions/FloatExtensions.cs
@@ -2,8 +2,17 @@ namespace RSLib.Extensions
 {
     public static class FloatExtensions
     {
-        #region ROUND
-
+        /// <summary>
+        /// Returns the fractional part of a value.
+        /// For example, the fractional part of 5.64f is 0.64f.
+        /// </summary>
+        /// <param name="value">Source value to get the fractional part of.</param>
+        /// <returns>Fractional part of the given value.</returns>
+        public static float Frac(this float value)
+        {
+            return value - System.MathF.Floor(value);
+        }
+        
         /// <summary>
         /// Rounds a value's decimal part to its nearest fraction (half, third, quarter, etc. based on denominator).
         /// For example, rounding 1.6f with a denominator of 2 will return 1.5f. With a denominator of 3, it will return 1.666f.
@@ -15,7 +24,5 @@ namespace RSLib.Extensions
         {
             return System.MathF.Round(value * denominator, System.MidpointRounding.AwayFromZero) / denominator;
         }
-
-        #endregion // ROUND
     }
 }
