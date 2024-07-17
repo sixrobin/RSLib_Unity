bcad7a4a8b59324e867075595585d419c9e40cd4
Robin Six <six.robin@hotmail.fr>
2024-07-17 11:22:12 +0200

    Added DrawGizmosType enum

---
+++ RSLib/Scripts/Debug/DrawGizmosType.cs
@@ -0,0 +1,9 @@
+namespace RSLib.Debug
+{
+    public enum DrawGizmosType
+    {
+        [UnityEngine.InspectorNameAttribute("Never")]    NEVER,
+        [UnityEngine.InspectorNameAttribute("Selected")] SELECTED,
+        [UnityEngine.InspectorNameAttribute("Always")]   ALWAYS
+    }
+}
